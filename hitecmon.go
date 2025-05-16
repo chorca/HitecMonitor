7591db322dadfa48805ce63d39d8b40607bb7e21
Chorca <chorca@gmail.com>
2025-05-15 21:03:24 -0400

    Fix fetching old readings

--- hitecmon.go
+++ hitecmon.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 
 	influxdb2 "github.com/influxdata/influxdb-client-go"
 )
@@ -43,6 +44,9 @@ func main() {
 
 	serByte := make([]byte, 1)
 	var lineData []byte
+	var lastTime time.Time
+	// Pre-populate to avoid throwing out the first result
+	lastTime = time.Now()
 	printBanner()
 	for {
 		_, err := serPort.Read(serByte)
@@ -56,7 +60,25 @@ func main() {
 		if serByte[0]^0x80 == 0xFB && len(lineData) > 1 && lineData[len(lineData)-2] == 0xFD {
 			/* Found the start byte
 			This means it's time to dump what we have and clear out for
-			the next bit of data */
+			the next bit of data.
+			However, sometimes it seems that the unit stores a reading in it's buffer
+			after a cycle finishes and spits it out after the 'stop' button is pressed.
+			To help eliminiate this, a simple timer.*/
+
+			// Unit updates at 1 or 2s intervals, this should be safe.
+			if time.Now().Sub(lastTime) > (time.Second * 5) {
+				// Throw out this reading
+				fmt.Println("Throwing out old reading")
+				lineData = nil
+				fmt.Printf("Current time %s\n", time.Now().String())
+				fmt.Printf("last time: %s\n", lastTime.String())
+				fmt.Printf("Difference: %s\n", time.Now().Sub(lastTime).String())
+				lastTime = time.Now()
+				continue
+			}
+
+			// Log the current time
+
 			lineData = append([]byte{0xFD, 0xFB}, lineData[:len(lineData)-2]...)
 			if len(lineData) == 76 {
 				parsedData := processData(lineData)
@@ -76,7 +98,8 @@ func main() {
 			}
 			// Reset the line in prep for data
 			lineData = nil
-
+			// Reset time
+			lastTime = time.Now()
 		}
 	}
 }
